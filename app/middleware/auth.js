f493b9edb83cf703b600ab16e972d8d8549906d5
James Billingham <james@billingham.net>
2016-02-20 16:34:51 +0000

    Use one cookie for all auth stuff

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -13,34 +13,33 @@ export default async function (req, res, next) {
 }
 
 async function handle(req, res) {
-	let githubId = req.signedCookies.githubId;
-	let githubToken = req.signedCookies.githubToken;
+	let auth = req.signedCookies.auth;
 
 	if (req.path === '/github_callback') {
 		const token = await Q.ninvoke(Octonode.auth, 'login', req.query.code);
 		const ghme = Octonode.client(token).me();
 		const [info] = await Q.ninvoke(ghme, 'info');
 
-		githubId = info.id;
-		githubToken = token;
+		auth = {
+			githubId: info.id,
+			githubToken: token,
+		};
 	}
 
-	if (!githubId || !githubToken || req.path === '/github_auth') {
+	if (!auth || req.path === '/github_auth') {
 		const redirUrl = Octonode.auth.login(['public_repo']);
 		res.redirect(redirUrl);
 		return;
 	}
 
-	const cookieOpts = { signed: true, secure: true, httpOnly: true };
-	res.cookie('githubId', githubId, cookieOpts);
-	res.cookie('githubToken', githubToken, cookieOpts);
+	res.cookie('auth', auth, { signed: true, secure: true, httpOnly: true });
 
 	if (req.path === '/github_callback') {
 		res.redirect('/');
 		return;
 	}
 
-	req.auth = { githubId, githubToken };
+	req.auth = auth;
 
 	return 'next';
 }
